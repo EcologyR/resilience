fe60f82e9109339aade3afbc0722e4821140b56b
VeruGHub <verukigoku@hotmail.com>
2023-07-04 18:14:09 +0200

    varios formatos de fecha, diferentes lag pre y post, datos de ejemplo en formato adecuado

--- R/resilience.R
+++ R/resilience.R
@@ -1,12 +1,11 @@
-
 #' Resilience indices
 #'
-#' @param series A [matrix] or [data.frame] representing a temporal series, with two columns: the dates (Date) and the performance (Numeric).
+#' @param series A [matrix] or [data.frame] representing a temporal series, with two columns: the dates (Character) and the performance (Numeric).
 #' If `series` is a matrix, it must have only two columns: the first with dates and the second with performance data.
 #' If `series` is a data.frame, it must contain at least two columns named `temp` and `performance`
-#' with date and performance values, respectively.
-#' @param event Date. Vector of dates of the disturbance events
-#' @param lag Vector. Number of temporal steps to include in the calculations, pre and post disturbance
+#' with date and performance values, respectively. temp must be in a format "YYYY-MM-DD", "YYYY-MM", "YYYY".
+#' @param event Character. List of dates or periods of the disturbance events.
+#' @param lag Vector. Number of years, months or days to include in the calculations, pre and post disturbance
 #' @param index Character. Indices to be calculated ('resilience', 'resistance' or 'recovery').
 #' Various elements can be concatenated in the vector.
 #'
@@ -15,13 +14,11 @@
 #'
 #' @examples
 # data(exampledata)
-# for (i in 1:length(unique(exampledata$ID))) {
-# item <- exampledata[exampledata$ID == unique(exampledata$ID)[i], ]
-# print(resindex(series = item,
-#          event = c(1990, 2000),
-#          lag = c(3, 3),
-#         index = c("resilience", "recovery", "resistance")))
-# }
+#' #
+# tapply(exampledata, INDEX = exampledata$ID, resindex,
+#        event = list("1990", "2000:2002"),
+#        lag = c(4, 3),
+#        index = c("resilience", "recovery", "resistance"))
 
 resindex <- function(series, event, lag, index) {
   ## Check arguments
@@ -31,7 +28,7 @@ resindex <- function(series, event, lag, index) {
     stop("series must be either a matrix or a data.frame")
   }
 
-  if (!inherits(series, "matrix")) {
+  if (inherits(series, "matrix")) {
     stopifnot(ncol(series) == 3)
   }
 
@@ -40,8 +37,8 @@ resindex <- function(series, event, lag, index) {
   stopifnot("performance" %in% names(series))
 
   # event
-  stopifnot(event >= 1)
-  stopifnot(class(event[1]) != class(series$temp[1]))
+  stopifnot(inherits(event, "list"))
+  stopifnot(length(event) >= 1)
 
   # lag
   stopifnot(length(lag) == 2)
@@ -57,29 +54,28 @@ resindex <- function(series, event, lag, index) {
 
 
   if ("resilience" %in% index) {
-    out_resil <- data.frame(event = event)
+    out_resil <- data.frame(event = unlist(event))
     out_resil$resilience <- do.call(rbind, lapply(list_of_periods, resil))
   } else {
     out_resil <- NULL
   }
 
   if ("resistance" %in% index) {
-    out_resis <- data.frame(event = event)
+    out_resis <- data.frame(event = unlist(event))
     out_resis$resistance <- do.call(rbind, lapply(list_of_periods, resist))
-
   } else {
     out_resis <- NULL
   }
 
   if ("recovery" %in% index) {
-    out_recov <- data.frame(event = event)
+    out_recov <- data.frame(event = unlist(event))
     out_recov$recovery <- do.call(rbind, lapply(list_of_periods, recov))
   } else {
     out_recov <- NULL
   }
 
   out_l <- list(out_resil, out_resis, out_recov)
-  out <- data.frame(event = event)
+  out <- data.frame(event = unlist(event))
 
   for (i in 1:3) {
     if (is.null(out_l[[i]])) next
@@ -92,21 +88,75 @@ resindex <- function(series, event, lag, index) {
 
 # splitting temporal series
 
-splitting <- function (event1, series1, lag1) {
+splitting <- function(event1, series1, lag1) {
+  if (grepl("[0-9]{4}-[0-9]{2}", series1[, "temp"][1])) {
+    lag1 <- months(lag1)
+  } else {
+    lag1 <- lag1
+  }
 
-  pre_period <- series1[series1$temp >= (event1 - lag1[1]) &
-                          series1$temp < event1, ]
+  series1[, "temp"] <- temp_to_date(series1[, "temp"])
 
-  disturb_period <- series1[series1$temp == event1, ]
+  if (!grepl(":", event1)) {
+    event1 <- temp_to_date(event1)
 
-  pos_period <- series1[series1$temp > event1 &
-                           series1$temp <= (event1 + lag1[2]), ]
+    pre_period <- series1[series1[, "temp"] >= (event1 - lag1[1]) &
+      series1[, "temp"] < event1, ]
 
-  return(list("pre_period" = pre_period,
-              "disturb_period" = disturb_period,
-              "pos_period" = pos_period))
+    disturb_period <- series1[series1[, "temp"] == event1, ]
 
+    pos_period <- series1[series1[, "temp"] > event1 &
+      series1[, "temp"] <= (event1 + lag1[2]), ]
+  } else {
+    event2 <- strsplit(event1, split = ":")[[1]]
+
+    event2 <- temp_to_date(event2)
+
+    pre_period <- series1[series1[, "temp"] >= (event2[1] - lag1[1]) &
+      series1[, "temp"] < event2[1], ]
+
+    disturb_period <- series1[series1[, "temp"] >= event2[1] &
+      series1[, "temp"] < event2[2], ]
+
+    pos_period <- series1[series1[, "temp"] > event2[2] &
+      series1[, "temp"] <= (event2[2] + lag1[2]), ]
+  }
+
+  return(list(
+    "pre_period" = pre_period,
+    "disturb_period" = disturb_period,
+    "pos_period" = pos_period
+  ))
 }
+
+
+# periods to Date format
+
+temp_to_date <- function(period) {
+  stopifnot(length(period) >= 1)
+
+  stopifnot(is.character(period))
+
+  if (grepl("[0-9]{4}-[0-9]{2}-[0-9]{2}", period[1])) {
+    return(as.Date(period))
+  } else {
+    if (grepl("[0-9]{4}-[0-9]{2}", period[1])) {
+      return(as.Date(paste0(period, "-01")))
+    } else { # years
+      return(as.numeric(period))
+    }
+  }
+}
+
+
+# ## check correct format ("YYYY-MM-DD")
+# apply(ini.fin, c(1, 2), function(x) {
+#
+#   if (!grepl("[0-9]{4}-[0-9]{2}-[0-9]{2}", x))
+#
+#     stop("Please provide dates as 'YYYY-MM-DD'")
+#
+# })
 
 
 # resilience
--- data-raw/data_prep.R
+++ data-raw/data_prep.R
@@ -4,4 +4,6 @@ exampledata <- read.csv("data-raw/DendroData.csv")
 exampledata <- subset(exampledata, select = c("ID", "Year", "BAI"))
 colnames(exampledata) <- c("ID" ,"temp", "performance")
 
+exampledata$temp <- as.character(exampledata$temp)
+
 usethis::use_data(exampledata, overwrite = TRUE)
